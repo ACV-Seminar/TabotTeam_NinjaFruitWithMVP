a9ef7873c01ea74cb4fba42c06b2b2b4a2d9b3cc
chuongdp <chuongdp161@gmail.com>
2018-08-08 18:00:57 +0700

    feature #104 create mvp base presenter

---
+++ Assets/Scripts/Core/BaseMVP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVP
+{
+    public abstract class BaseMVP<P> : UnityEngine.MonoBehaviour, IView where P : IPresenter, new()
+    {
+        protected P presenter;
+
+        void Awake()
+        {
+            presenter = new P();
+            Init();
+        }
+
+        protected abstract void Init();
+
+        public abstract void BindData();
+
+        public abstract void BindModel<T>(T data);
+    }
+}
